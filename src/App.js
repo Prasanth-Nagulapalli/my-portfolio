e254631084d68a59777ce4b9385e820f61b70df9
Prasanth-Nagulapalli <prasanth.ragava@gmail.com>
2024-07-14 20:40:45 +0530

    hero section completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
-
+import Header from './components/Header'
 function App() {
   return (
     <>
       <Navbar />
+      <Header />
+      <div style={{height:"500vh"}}>
+
+      </div>
     </>
   );
 }
