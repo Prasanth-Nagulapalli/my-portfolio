45e5d75a053cd52afdd694d9ec047b95239b3880
Prasanth-Nagulapalli <prasanth.ragava@gmail.com>
2024-07-14 21:31:28 +0530

    services added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
+import Services from "./components/Services";
 function App() {
   return (
     <>
       <Navbar />
       <Header />
       <About />
+      <Services />
       <div style={{ height: "500vh" }}></div>
     </>
   );
