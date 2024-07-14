0c03c447d63a8198f82b7a551233ddc91ed86b3b
Prasanth-Nagulapalli <prasanth.ragava@gmail.com>
2024-07-14 14:56:33 +0530

    nav index.css added

---
+++ src/images/index.js
@@ -0,0 +1,8 @@
+export {default as profile1} from './profile-1.png'
+export {default as profile2 } from './profile-2.jpg'
+export {default as profile3 } from './profile-3.jpg'
+export {default as profile4 } from './profile-4.jpg'
+export {default as profile5 } from './profile-5.jpg'
+export {default as profile6 } from './profile-6.jpg'
+
+
